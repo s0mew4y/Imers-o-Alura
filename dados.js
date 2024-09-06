3fa00deaec69ef19e14e718f8b26d2377726feea
Rafael de Freitas <167362524+s0mew4y@users.noreply.github.com>
2024-09-06 15:58:12 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Frank Ocean",
+        descricao: "Figura influente na música alternativa, com um estilo único e letras poéticas que exploram temas de amor, perda e identidade.",
+        link: "https://pt.wikipedia.org/wiki/Frank_Ocean",
+        tags: "franck ocean francisco oceano Franck Ocean"
+    },
+    {
+        titulo: "Kendrick Lamar",
+        descricao: "Considerado um dos maiores rappers de sua geração, com letras socialmente conscientes e um fluxo lírico impecável.",
+        link: "https://pt.wikipedia.org/wiki/Kendrick_Lamar",
+        tags: "kendrick lamar Kendrick Lamar"
+    },
+    {
+        titulo: "J.Cole",
+        descricao: "Um dos maiores nomes do hip hop atual, conhecido por suas letras introspectivas e habilidades de produção musical.",
+        link: "https://pt.wikipedia.org/wiki/J._Cole",
+        tags: "JCole jcole Jcole"
+    }
+];
